f73cb35be113ee2205004287623fb6066f6d3c8a
umgi <gtdsocial@icloud.com>
2022-04-13 18:26:25 +0300

    add: d02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -18,6 +27,89 @@ package main
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func Unpack(input string) (string, error) {
+	if len(input) == 0 {
+		return "", nil
+	}
 
+	// Обрабатываем кейс с первой цифрой. Так как '0'..'9' занимают один байт в utf-8.
+	// То достаточно проверить первый байт строки
+	if '0' <= input[0] && input[0] <= '9' {
+		return "", errors.Errorf("first char can't be number")
+	}
+
+	isEscape := false
+	wasLetter := false
+	var result strings.Builder
+	var char rune
+	var count int
+	for idx, ch := range input {
+		isNumber := '0' <= ch && ch <= '9'
+		isSlash := '\\' == ch
+
+		if isEscape {
+			if !isSlash && !isNumber {
+				return "", errors.Errorf("escaped %c at %d position", ch, idx)
+			}
+			char = ch
+			count = 0
+			wasLetter = true
+			isEscape = false
+			continue
+		}
+
+		// Вывод происходит только когда встретили новую букву.
+		// Чтобы обработать a0b и ab, сохраняем что последний раз был символ,
+		// и печатаем либо указанное количество в count, либо 1, так как при ab, count == 0
+		if !isNumber && idx > 0 {
+			if wasLetter {
+				count = 1
+			}
+			result.WriteString(strings.Repeat(string(char), count))
+		}
+
+		if isSlash && !isEscape {
+			isEscape = true
+			continue
+		}
+
+		if isNumber {
+			count = count*10 + int(ch-'0')
+			wasLetter = false
+			continue
+		}
+
+		char = ch
+		count = 0
+		wasLetter = true
+	}
+
+	// Вывод происходит всегда перед обработкой,
+	// поэтому необходимо вывести результат последней итерации.
+	if !isEscape {
+		if wasLetter {
+			count = 1
+		}
+		result.WriteString(strings.Repeat(string(char), count))
+	}
+
+	return result.String(), nil
+}
+
+// echo "a2b3c4" | go run .
+// aabbbcccc
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		result, err := Unpack(scanner.Text())
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "unpack error:", err)
+			os.Exit(2)
+		}
+		fmt.Println(result)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		os.Exit(1)
+	}
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,69 @@
 package main
+
+import "testing"
+
+func TestUnpack(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+		err      bool
+	}{
+		{"", "", false},
+		{"a", "a", false},
+		{"ab", "ab", false},
+
+		{"a0", "", false},
+		{"a00", "", false},
+		{"a0b0", "", false},
+
+		{"abc", "abc", false},
+		{"a4bc", "aaaabc", false},
+		{"a12bc", "aaaaaaaaaaaabc", false},
+		{"a12b5c", "aaaaaaaaaaaabbbbbc", false},
+
+		{"\\11", "1", false},
+		{"\\18", "11111111", false},
+
+		{"\\\\3", "\\\\\\", false},
+
+		{"\\a8", "anything", true},
+		{"45", "anything", true},
+		{"\\", "", false},
+		{"\\\\", "\\", false},
+		{"\\\\\\", "\\", false},
+		{"\\\\\\\\", "\\\\", false},
+
+		{"a4bc2d5e", "aaaabccddddde", false},
+		{"abcd", "abcd", false},
+		{"45", "", true},
+
+		{"qwe\\4\\5", "qwe45", false},
+		{"qwe\\45", "qwe44444", false},
+		{"qwe\\\\5", "qwe\\\\\\\\\\", false},
+
+		{"a\\a0", "", true},
+		{"\\00", "", false},
+		{"\\06", "000000", false},
+		{"\\020", "00000000000000000000", false},
+		{"f0001", "f", false},
+	}
+
+	for _, test := range tests {
+		result, err := Unpack(test.input)
+		if test.err {
+			if err == nil {
+				t.Logf("for %s expected err, got result: %s", test.input, result)
+				t.Fail()
+			}
+
+		} else {
+			if err != nil {
+				t.Logf("for %s expected %s, got error: %s", test.input, test.expected, err)
+				t.Fail()
+			} else if result != test.expected {
+				t.Logf("for %#v expected %#v, got result: %#v", test.input, test.expected, result)
+				t.Fail()
+			}
+		}
+	}
+}
