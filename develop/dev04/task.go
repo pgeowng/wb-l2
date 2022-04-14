71d9b2691a0e92b91d0eeaf38ca0d1f7c7bbafd0
umgi <gtdsocial@icloud.com>
2022-04-14 22:59:14 +0300

    add: d04

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
 /*
 === Поиск анаграмм по словарю ===
 
@@ -19,6 +29,106 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+// Основная идея заключается в присвоении номера для любой комбинации
+// букв из русского алфавита. Выполняется за O(n).
+// ""   = 0
+// "а"  = 1
+// "я"  = 32
+// "ё"  = 33
+// "аа" = 34
+// "аё" = "ёа" = 66
+// "ёё" = 98
+// "ааа" = 99
+func hashLettersRussian(word string) (uint64, error) {
+	if len(word) == 0 {
+		return 0, nil
+	}
+	const base = 32
 
+	var key uint64
+	var size uint64 = 0
+	for idx, r := range word {
+		if r == '\u0451' {
+			key += 32
+		} else if r < '\u0430' || r > '\u044F' {
+			return 0, errors.Errorf("letter %c at %d is not from russian alphabet", r, idx)
+		} else {
+			key += uint64(r - '\u0430')
+		}
+		size++
+	}
+	key += (size - 1) + (size-1)*size/2*base + 1
+
+	return key, nil
+}
+
+// Для каждого слова вычисляем его хеш.
+// Сохраняем в dict map, проверяя уникальность слова.
+// Затем по сортируем каждое множество
+// Сложность: пусть n - кол-во всех слов, m - макс длина слова
+// Если все в одной группе: O(n*m) + O(n*logn)
+// Если все в разных группах: O(n*m) + O(n)
+func groupAnagrams(list []string) map[string][]string {
+	dict := map[uint64][]string{}
+	uniq := map[string]struct{}{}
+
+	for _, word := range list {
+		if len(word) != 0 {
+			lword := strings.ToLower(word)
+			_, ok := uniq[lword]
+			if ok {
+				continue
+			}
+			key, err := hashLettersRussian(lword)
+			if err != nil {
+				continue
+			}
+			dict[key] = append(dict[key], lword)
+			uniq[lword] = struct{}{}
+		}
+	}
+
+	result := map[string][]string{}
+	for _, set := range dict {
+		if len(set) > 1 {
+			result[set[0]] = set
+			sort.Strings(set)
+		}
+	}
+
+	return result
+}
+
+// $ echo "лягушка  гушялка гав ваг гва  а" | go run .
+// гав (3): ваг, гав, гва
+// лягушка (2): гушялка, лягушка
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	input := []string{}
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading input:", err)
+	}
+
+	result := groupAnagrams(input)
+
+	if len(result) == 0 {
+		fmt.Println("no groups")
+		return
+	}
+
+	keys := make([]string, 0, len(result))
+
+	for k := range result {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		words := result[k]
+		fmt.Printf("%s (%d): %s\n", k, len(words), strings.Join(words, ", "))
+	}
 }
--- develop/dev04/task_test.go
+++ develop/dev04/task_test.go
@@ -1,1 +1,107 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHashLettersRussian(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected uint64
+		err      bool
+	}{
+		{"english", 0, true},
+
+		{"", 0, false},
+		{"а", 1, false},
+		{"я", 32, false},
+		{"ё", 33, false},
+		{"аа", 34, false},
+		{"ая", 65, false},
+		{"яа", 65, false},
+		{"аё", 66, false},
+		{"ёа", 66, false},
+		{"яя", 96, false},
+		{"ёё", 98, false},
+		{"ааа", 99, false},
+	}
+
+	for _, test := range tests {
+		result, err := hashLettersRussian(test.input)
+
+		if test.err {
+			if err == nil {
+				t.Logf("for %#v expected error, got %d", test, result)
+				t.Fail()
+			}
+		} else {
+			if err != nil {
+				t.Logf("for %#v expected result, got error %s", test, err)
+				t.Fail()
+			} else if result != test.expected {
+				t.Logf("for %#v expected result %d doesn't match %d", test, test.expected, result)
+				t.Fail()
+			}
+		}
+	}
+}
+
+func TestGroupAnagrams(t *testing.T) {
+	tests := []struct {
+		input  []string
+		result map[string][]string
+	}{
+		{
+			[]string{"абвг", "абв"},
+			map[string][]string{},
+		},
+		{
+			[]string{"listen", "silent"},
+			map[string][]string{},
+		},
+		{
+			[]string{"лягушка", "гуляшка", "человек"},
+			map[string][]string{
+				"лягушка": []string{"гуляшка", "лягушка"},
+			},
+		},
+		{
+			[]string{"гуляшка", "лягушка", "человек"},
+			map[string][]string{
+				"гуляшка": []string{"гуляшка", "лягушка"},
+			},
+		},
+		{
+			[]string{"ГуЛяШка", "ляГуШка", "лягуШкА"},
+			map[string][]string{
+				"гуляшка": []string{"гуляшка", "лягушка"},
+			},
+		},
+		{
+			[]string{"сева", "веса", "сёва", "васё"},
+			map[string][]string{
+				"сева": []string{"веса", "сева"},
+				"сёва": []string{"васё", "сёва"},
+			},
+		},
+		{
+			[]string{"столик", "пятак", "реализме", "тяпка", "пятка", "израелем", "пятак", "листок", "слово", "тяпка", "слиток"},
+			map[string][]string{
+				"столик":   []string{"листок", "слиток", "столик"},
+				"пятак":    []string{"пятак", "пятка", "тяпка"},
+				"реализме": []string{"израелем", "реализме"},
+			},
+		},
+	}
+	for _, test := range tests {
+		result := groupAnagrams(test.input)
+
+		if fmt.Sprint(result) != fmt.Sprint(test.result) {
+			t.Logf("unexpected result")
+			t.Logf("expected %v", test.result)
+			t.Logf("got %v", result)
+			t.Fail()
+		}
+	}
+}
